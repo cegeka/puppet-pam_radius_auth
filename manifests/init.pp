ee55efe9c337757c555d00ecbf85711575ea21c5
Roger Ignazio <rignazio@gmail.com>
2013-01-25 19:13:41 -0600

    Added EL5 support for sshd and sudo PAM modules

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,16 @@
+# This is the rji/pam_radius_auth module for Puppet.
+# Written by Roger Ignazio <rignazio at gmail dot com>
+# https://github.com/rji/puppet-modules/tree/master/pam_radius_auth
+#
+# Configuration:
 # $pam_radius_servers, $pam_radius_secret, and $pam_radius_timeout should
-# be modified for your environment. See the README for more information.
+# be modified for your environment. The 'redhat-lsb' package will be installed
+# to determine the OS major release.
+#
+# Compatibility:
+# This module should also handle newer (or older) EL releases, or other distros,
+# with slight modification (eg. putting the pam_radius_auth config in your
+# system's relevant PAM file(s)). See GitHub or the README for more information.
 #
 class pam_radius_auth($pam_radius_servers = [ "192.168.10.8",
                                               "192.168.10.9" ],
@@ -9,13 +20,35 @@ class pam_radius_auth($pam_radius_servers = [ "192.168.10.8",
   # Distribution check
   case $operatingsystem {
     centos, redhat: {
-      $supported     = true
-      $pkg           = "pam_radius"
-      $conf          = "/etc/pam_radius.conf"
-      $pam_sshd_conf = "pam_sshd_el"
-      $pam_sudo_conf = "pam_sudo_el"
+      # Vars that apply to all Enterprise Linux releases
+      $pkg  = "pam_radius"
+      $conf = "/etc/pam_radius.conf"
+
+      # The 'redhat-lsb' package is required to get the  major release number
+      package { "redhat-lsb":
+        ensure => present,
+      }
+
+      case $lsbmajdistrelease {
+        5: {
+          $supported     = true
+          $pam_sshd_conf = "pam_sshd_el5"
+          $pam_sudo_conf = "pam_sudo_el5"
+        }
+        6: {
+          $supported     = true
+          $pam_sshd_conf = "pam_sshd_el6"
+          $pam_sudo_conf = "pam_sudo_el6"
+        }
+        default: {
+          $supported = false
+          notify { "pam_radius_auth module not supported on CentOS ${lsbmajdistrelease}":}
+        }
+      }
     }
     ubuntu, debian: {
+      # This module has been tested with Ubuntu 12.04 LTS.
+      # Your experience may differ on older releases.
       $supported     = true
       $pkg           = "libpam-radius-auth"
       $conf          = "/etc/pam_radius_auth.conf"
@@ -24,10 +57,11 @@ class pam_radius_auth($pam_radius_servers = [ "192.168.10.8",
     }
     default: {
       $supported = false
-      notify { "pam_radius_auth module not supported on ${operatingsystem}":}
+      notify { "pam_radius_auth module not supported on OS ${operatingsystem}":}
     }
   }
 
+  # Environment checks passed, let's continue.
   if ($supported == true) {
     # Package installation
      # On Redhat/CentOS, pam_radius is in the EPEL repo
