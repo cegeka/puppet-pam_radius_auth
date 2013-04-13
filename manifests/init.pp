60a8b44b41663090e7935f1911c824065dc46ae4
Roger Ignazio <rignazio@gmail.com>
2013-04-13 17:26:30 -0500

    apply puppet-lint style recommendations

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,33 +11,33 @@
 # with slight modification (eg. putting the pam_radius_auth config in your
 # system's relevant PAM file(s)). See GitHub or the README for more information.
 #
-class pam_radius_auth($pam_radius_servers = [ "192.168.10.8",
-                                              "192.168.10.9" ],
-                      $pam_radius_secret  = "secret",
+class pam_radius_auth($pam_radius_servers = [ '192.168.10.8',
+                                              '192.168.10.9' ],
+                      $pam_radius_secret  = 'secret',
                       $pam_radius_timeout = '3' )
 {
   # Distribution check
   case $operatingsystem {
     centos, redhat: {
       # Vars that apply to all Enterprise Linux releases
-      $pkg  = "pam_radius"
-      $conf = "/etc/pam_radius.conf"
+      $pkg  = 'pam_radius'
+      $conf = '/etc/pam_radius.conf'
 
       # The 'redhat-lsb' package is required to get the  major release number
-      package { "redhat-lsb":
+      package { 'redhat-lsb':
         ensure => present,
       }
 
       case $lsbmajdistrelease {
         5: {
           $supported     = true
-          $pam_sshd_conf = "pam_sshd_el5"
-          $pam_sudo_conf = "pam_sudo_el5"
+          $pam_sshd_conf = 'pam_sshd_el5'
+          $pam_sudo_conf = 'pam_sudo_el5'
         }
         6: {
           $supported     = true
-          $pam_sshd_conf = "pam_sshd_el6"
-          $pam_sudo_conf = "pam_sudo_el6"
+          $pam_sshd_conf = 'pam_sshd_el6'
+          $pam_sudo_conf = 'pam_sudo_el6'
         }
         default: {
           $supported = false
@@ -49,10 +49,10 @@ class pam_radius_auth($pam_radius_servers = [ "192.168.10.8",
       # This module has been tested with Ubuntu 12.04 LTS.
       # Your experience may differ on older releases.
       $supported     = true
-      $pkg           = "libpam-radius-auth"
-      $conf          = "/etc/pam_radius_auth.conf"
-      $pam_sshd_conf = "pam_sshd_deb"
-      $pam_sudo_conf = "pam_sudo_deb"
+      $pkg           = 'libpam-radius-auth'
+      $conf          = '/etc/pam_radius_auth.conf'
+      $pam_sshd_conf = 'pam_sshd_deb'
+      $pam_sudo_conf = 'pam_sudo_deb'
     }
     default: {
       $supported = false
@@ -63,8 +63,8 @@ class pam_radius_auth($pam_radius_servers = [ "192.168.10.8",
   # Environment checks passed, let's continue.
   if ($supported == true) {
     # Package installation
-     # On Redhat/CentOS, pam_radius is in the EPEL repo
-     # On Debian/Ubuntu, libpam-radius-auth is included in main
+      # On Redhat/CentOS, pam_radius is in the EPEL repo
+      # On Debian/Ubuntu, libpam-radius-auth is included in main
     package { $pkg:
       ensure  => present,
     }
@@ -73,26 +73,26 @@ class pam_radius_auth($pam_radius_servers = [ "192.168.10.8",
       ensure  => present,
       owner   => 'root',
       group   => 'root',
-      mode    => 0600,
-      content => template("pam_radius_auth/pam_radius.conf.erb"),
+      mode    => '0600',
+      content => template('pam_radius_auth/pam_radius.conf.erb'),
       require => Package[$pkg],
     }
 
     # Copy sshd and sudo files to /etc/pam.d
-    file { "/etc/pam.d/sshd":
+    file { '/etc/pam.d/sshd':
       ensure  => present,
       owner   => 'root',
       group   => 'root',
-      mode    => 0644,
+      mode    => '0644',
       source  => "puppet:///modules/pam_radius_auth/${pam_sshd_conf}",
       require => [ Package[$pkg], File[$conf] ],
     }
 
-    file { "/etc/pam.d/sudo":
+    file { '/etc/pam.d/sudo':
       ensure  => present,
       owner   => 'root',
       group   => 'root',
-      mode    => 0644,
+      mode    => '0644',
       source  => "puppet:///modules/pam_radius_auth/${pam_sudo_conf}",
       require => [ Package[$pkg], File[$conf] ],
     }
